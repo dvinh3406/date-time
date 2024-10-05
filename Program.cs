78584b33d7c60baf124145d6af1bcd4a8b4fa500
dvinh3406 <pdv206@gmail.com>
2024-10-05 14:34:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime dateTime= DateTime.Now;
+        Console.WriteLine("{0}{1}{2}", dateTime.Hour, dateTime.Minute, dateTime.Second);
+    }
+}
